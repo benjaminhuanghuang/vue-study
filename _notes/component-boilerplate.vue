da4ad1d3c31b15fa9c8dd9d826d5a8604f5b62d0
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-12-09 14:03:29 -0800

    update

---
+++ _notes/component-boilerplate.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ user.username }}</h1>
+
+    <div v-if="user.isAdmin"></div>
+    
+    <div v-if="{item, index} in user.friends" :key="item.id">
+      {{item}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  date() {
+    return {
+      user:{
+
+      }
+    }
+  },
+  watch: {
+    
+  },
+  computed:{},
+  methods:{}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
