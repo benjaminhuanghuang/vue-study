93cc2f43b9e900140b226150e91092daef513529
benjaminhuang <benjaminhuanghuang@gmail.com>
2024-11-03 17:45:07 -0800

    vue3 code

---
+++ Vue3-SourceCode/vue3-yarn-rollup/scripts/build.js
@@ -0,0 +1,32 @@
+/*
+    node build.js <package> -f <format>
+*/
+import minimist from "minimist";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+import esbuild from "esbuild";
+
+const args = minimist(process.argv.slice(2));
+const __filename = fileURLToPath(import.meta.url);  // 'file:///C:/path/to/file.txt' -> 'C:/path/to/file.txt'
+// node esm module does not have __dirname
+const __dirname = dirname(__filename);
+// node esm module does not have require
+const require = createRequire(import.meta.url);
+
+async function build(target) {
+  await execArgv('rollup', ['-c', '--environment', `TARGET:${target}`]);
+}
+
+async function runParallel(dirs, buildFn) {
+  const ps = [];
+  for (const target of targets) {
+    ps.push(buildFn(target));
+  }
+  // wait for all the promises to resolve
+  await Promise.all(ps);
+}
+
+runParallel(dirs, build.then(() => {
+  console.log("Build successfully");
+}));
