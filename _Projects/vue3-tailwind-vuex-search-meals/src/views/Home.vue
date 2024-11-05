342b0d1031231384c1829c2653a5b3981f838ef9
benjaminhuang <benjaminhuanghuang@gmail.com>
2024-11-04 20:32:59 -0800

    update

--- _Projects/vue3-tailwind-vuex-search-meals/src/views/Home.vue
+++ _Projects/vue3-tailwind-vuex-search-meals/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-            home
+    <div class="p-8 pb-0 text-orange-500">
+        <h1 class="text-4xl font-bold mb-4">Random Meals</h1>
+    </div>
+    <Meals :meals="meals" />
 </template>
-    
+
+
 <script setup lang='ts'>
-    import {computed} from 'vue';
-    import store from '../store';
+import { computed } from 'vue';
+import store from '../store';
 
 
-    const meals = computed(() => store.state.meals);
+const meals = computed(() => store.state.meals);
 
 
-    
+
 </script>
-    
-<style>
-    
-</style>
+
+<style></style>
