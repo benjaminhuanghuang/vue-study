d1fe1ec5119b515bc4b77400b68bcd1ebdc5e0b2
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-01-06 00:00:53 -0800

    update

--- _Courses/The Vue.js Master Class 2024 Edition/vuejs-masterclass-2024/database/seed.js
+++ _Courses/The Vue.js Master Class 2024 Edition/vuejs-masterclass-2024/database/seed.js
@@ -1,18 +1,36 @@
-import {fakerEN_US as faker} from "@faker-js/faker";
-import {createClient} from '@supabase/supabase-js'
+import { fakerEN_US as faker } from '@faker-js/faker'
+import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.VITE_SUPABASE_APP_URL, process.env.VITE_SUPABASE_SERVICE_ROLE_KEY)
+const supabase = createClient(
+  process.env.VITE_SUPABASE_APP_URL,
+  process.env.VITE_SUPABASE_SERVICE_ROLE_KEY
+)
 
 // console.log(supabase)
 
+// const seedProjects = async (num) => {
+//   const projects = []
+//   for (let i = 0; i < num; i++) {
+//     const name = faker.lorem.words(3)
+//     projects.push({
+//       name: name,
+//       slug: name.toLowerCase().replace(/ /g, '-'),
+//       status: faker.helpers.arrayElement(['in-progress', 'completed']),
+//       collaborators: faker.helpers.arrayElements([1, 2, 3])
+//     })
+//   }
+//   console.log(projects)
+//   await supabase.from('projects').insert(projects)
+// }
+
+// await seedProjects(10)
 const seedProjects = async () => {
     const name = faker.lorem.words(3)
     await supabase.from('projects').insert({
-        name,
+        name:name,
         slug: name.toLowerCase().replace(/ /g, '-'),
         status: faker.helpers.arrayElement(['in-progress', 'completed']),
-        collaborators: faker.helpers.arrayElements([1, 2, 3]),    
+        collaborators: faker.helpers.arrayElements([1, 2, 3]),
     });
 }
-
-await seedProjects();
+await seedProjects()
--- _Courses/The Vue.js Master Class 2024 Edition/vuejs-masterclass-2024/src/pages/projects/index.vue
+++ _Courses/The Vue.js Master Class 2024 Edition/vuejs-masterclass-2024/src/pages/projects/index.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts"> 
+import {supabase} from '@/lib/supabaseClient '
+const getProjects = async ()=>{
+  const {data, error} = await supabase.from('projects').select()
+  if(error){
+    console.error(error)
+  }
+}
 </script>
 
 
