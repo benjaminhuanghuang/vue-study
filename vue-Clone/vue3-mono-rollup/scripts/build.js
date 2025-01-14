e86e20758bc34fe5b230d611054ed9084e409c2b
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-01-14 09:55:55 -0800

    update

--- vue-Clone/vue3-mono-rollup/scripts/build.js
+++ vue-Clone/vue3-mono-rollup/scripts/build.js
@@ -7,15 +7,22 @@ import { fileURLToPath } from "url";
 import { createRequire } from "module";
 import esbuild from "esbuild";
 
+// Get all packages
+const fs = require("fs");
+const dirs = fs.readdirSync('packages').filter((fs) => {
+  return !fs.statSync(`packages/${p}`).isDirectory();
+});
+
+// Build
 const args = minimist(process.argv.slice(2));
-const __filename = fileURLToPath(import.meta.url);  // 'file:///C:/path/to/file.txt' -> 'C:/path/to/file.txt'
+const __filename = fileURLToPath(import.meta.url); // 'file:///C:/path/to/file.txt' -> 'C:/path/to/file.txt'
 // node esm module does not have __dirname
 const __dirname = dirname(__filename);
 // node esm module does not have require
 const require = createRequire(import.meta.url);
 
 async function build(target) {
-  await execArgv('rollup', ['-c', '--environment', `TARGET:${target}`]);
+  await execArgv("rollup", ["-c", "--environment", `TARGET:${target}`]);
 }
 
 async function runParallel(dirs, buildFn) {
@@ -27,6 +34,9 @@ async function runParallel(dirs, buildFn) {
   await Promise.all(ps);
 }
 
-runParallel(dirs, build.then(() => {
-  console.log("Build successfully");
-}));
+runParallel(
+  dirs,
+  build.then(() => {
+    console.log("Build successfully");
+  })
+);
