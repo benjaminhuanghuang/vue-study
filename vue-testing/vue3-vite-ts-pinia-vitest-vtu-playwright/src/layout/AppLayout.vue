a783a0a17c308cb4f4c0f86263599e6ac9a2a8e3
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-01-14 21:42:35 -0800

    update

---
+++ vue-testing/vue3-vite-ts-pinia-vitest-vtu-playwright/src/layout/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <TopNavbar />
+        <slot />
+    </div>
+</template>
+
+<script setup lang='ts'>
+import TopNavbar from './TopNavbar.vue' 
+</script>
+
+<style></style>
---
+++ vue-testing/vue3-vite-ts-pinia-vitest-vtu-playwright/src/layout/TopNavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+      <RouterLink :to="{ name: 'about' }">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  background-color: aqua;
+}
+
+nav {
+  display: inline-flex;
+  gap: 30px;
+  justify-content: center;
+
+}
+
+.router-link-active,
+.router-link-exact-active {
+  text-decoration: underline;
+}
+</style>
