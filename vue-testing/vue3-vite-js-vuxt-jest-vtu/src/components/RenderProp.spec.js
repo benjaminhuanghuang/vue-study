33c9ff9732dfe0f701ea87e5037c61e0a8fd8c50
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-01-15 21:59:58 -0800

    update

--- vue-testing/vue3-vite-js-vuxt-jest-vtu/src/components/RenderProp.spec.js
+++ vue-testing/vue3-vite-js-vuxt-jest-vtu/src/components/RenderProp.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "./HelloWorld.vue";
+// Component to test
+import HelloWorld from "./RenderProp.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
