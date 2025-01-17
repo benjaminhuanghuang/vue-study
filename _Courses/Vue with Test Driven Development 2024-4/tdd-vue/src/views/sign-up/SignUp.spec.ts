e0b7f6e7afedcb8523e5ad3125e06f18dcaf612d
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-01-16 21:54:09 -0800

    update

--- _Courses/Vue with Test Driven Development 2024-4/tdd-vue/src/views/sign-up/SignUp.spec.ts
+++ _Courses/Vue with Test Driven Development 2024-4/tdd-vue/src/views/sign-up/SignUp.spec.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+
 // Component to test
 import SignUp from './SignUp.vue';
 
@@ -8,5 +10,60 @@ describe('SignUp', () => {
     render(SignUp);
     const header = screen.getByRole('heading', { name: 'Sign Up' });
     expect(header).toBeInTheDocument();
+  });
+
+  it('has username input', () => {
+    render(SignUp);
+    expect(screen.getByLabelText('Username')).toBeInTheDocument();
+  });
+  it('has email input', () => {
+    render(SignUp);
+    expect(screen.getByLabelText('E-mail')).toBeInTheDocument();
+  });
+
+  it('has password input', () => {
+    render(SignUp);
+    expect(screen.getByLabelText('Password')).toBeInTheDocument();
+  });
+
+  it('has password type for password input', () => {
+    render(SignUp);
+    expect(screen.getByLabelText('Password')).toHaveAttribute('type', 'password');
+  });
+
+  it('has password repeat input', () => {
+    render(SignUp);
+    expect(screen.getByLabelText('Password Repeat')).toBeInTheDocument();
+  });
+
+  it('has password type for password repeat input', () => {
+    render(SignUp);
+    expect(screen.getByLabelText('Password Repeat')).toHaveAttribute('type', 'password');
+  });
+
+  it('has Sign up button', () => {
+    render(SignUp);
+    const button = screen.getByRole('button', { name: 'Sign Up' });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('disable button initially', () => {
+    render(SignUp);
+    expect(screen.getByRole('button', { name: 'Sign Up' })).toBeDisabled();
+  });
+
+  describe('when user sets same value for password inputs', () => {
+    it('enable button', async () => {
+      const user = userEvent.setup();
+      render(SignUp);
+
+      const passwordInput = screen.getByLabelText('password');
+      const passwordRepeatInput = screen.getByLabelText('password Repeat');
+
+      await user.type(passwordInput, '12345');
+      await user.type(passwordRepeatInput, '12345');
+
+      expect(screen.getByRole('button', { name: 'Sign Up' })).toBeEnabled();
+    });
   });
 });
--- _Courses/Vue with Test Driven Development 2024-4/tdd-vue/src/views/sign-up/SignUp.vue
+++ _Courses/Vue with Test Driven Development 2024-4/tdd-vue/src/views/sign-up/SignUp.vue
@@ -1,3 +1,37 @@
 <template>
     <h1>Sign Up</h1>
+    <div>
+        <label for="username">Username</label>
+        <input id="username" />
+    </div>
+    <div>
+        <label for="email">E-mail</label>
+        <input id="email" />
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input id="password" type="password" @input="onChangePassword" />
+    </div>
+    <div>
+        <label for="passwordRepeat">Password Repeat</label>
+        <input id="passwordRepeat" type="password" @input="onChangePasswordRepeat" />
+    </div>
+    <button :disabled="disabled">Sign Up</button>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+const password = ref('');
+const passwordRepeat = ref('');
+const disabled = ref(true);
+
+const onChangePassword = (event) => {
+    password.value = event.target.value;
+    disabled.value = password.value !== passwordRepeat.value;
+}
+
+const onChangePasswordRepeat = (event) => {
+    passwordRepeat.value = event.target.value;
+    disabled.value = password.value !== passwordRepeat.value;
+}
+</script>
