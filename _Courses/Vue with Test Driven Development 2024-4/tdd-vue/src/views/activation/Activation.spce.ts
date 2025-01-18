6d89b069544d17e8a87340db1dc963d66b22802c
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-01-17 22:19:40 -0800

    update

--- _Courses/Vue with Test Driven Development 2024-4/tdd-vue/src/views/activation/Activation.spce.ts
+++ _Courses/Vue with Test Driven Development 2024-4/tdd-vue/src/views/activation/Activation.spce.ts
@@ -1,31 +1,54 @@
 import { setupServer } from 'msw/node';
 import { HttpResponse, http } from 'msw';
-import { render, waitFor } from 'test/helper';
+import { render, waitFor, router } from 'test/helper';
 import { afterAll, beforeAll } from 'vitest';
 
 // Component to test
 import Activation from './Activation.vue';
 
 let counter = 0;
+let token: string | undefined;
+
 const server = setupServer(
-  http.post('/api/v1/users/:token/active', () => {
+  http.post('/api/v1/users/:token/active', ({ params }) => {
     counter++;
+    token = params.token as string;
     return HttpResponse.json({});
   })
 );
 beforeEach(() => {
   counter = 0;
+  token = undefined;
   server.resetHandlers();
 });
 
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 
+const setup = async (path: string) => {
+  router.push(path);
+  await router.isReady();
+  return render(Activation);
+};
+
 describe('Activation', () => {
   it('send activation request to server', async () => {
-    render(Activation);
+    await setup('/activation/123');
     await waitFor(() => {
       expect(counter).toBe(1);
+    });
+  });
+
+  describe.each([
+    {
+      activationToken: '123'
+    }
+  ])('when token is $token', ({ activationToken }) => {
+    it('send  token in request', async () => {
+      await setup(`/activation/${activationToken}`);
+      await waitFor(() => {
+        expect(token).toBe(activationToken);
+      });
     });
   });
 });
--- _Courses/Vue with Test Driven Development 2024-4/tdd-vue/src/views/activation/Activation.vue
+++ _Courses/Vue with Test Driven Development 2024-4/tdd-vue/src/views/activation/Activation.vue
@@ -5,9 +5,22 @@
 </template>
 
 <script setup lang='ts'>
+import axios from 'axios';
 import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
+route.params.token;
 
+onMounted(() => {
+    axios.patch(`/api/v1/users/${route.params.token}/active`)
+        .then(() => {
+            console.log('Activation successful');
+        })
+        .catch(() => {
+            console.error('Activation failed');
+        });
+});
 </script>
 
 <style></style>
